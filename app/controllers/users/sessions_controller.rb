e515166f2f0e9e397cd2a7280532b932e8701997
Ismail mayito <ismayito1@gmail.com>
2024-04-23 09:30:49 +0300

    Add user authentication on all controllers.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+    skip_action :authenticate_user!
 
   # GET /resource/sign_in
   # def new
